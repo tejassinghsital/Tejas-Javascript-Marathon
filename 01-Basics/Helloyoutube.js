bfb7853e79e1a8a0f0d69a2585a7a0e91f384b7d
Tejas Singh Sital <tejassingh1992001@gmail.com>
2022-06-17 02:52:57 +0530

    Dome with introduction and lexical structure stream on community dashboard and take specific notes too.

---
+++ 01-Basics/Helloyoutube.js
@@ -0,0 +1,65 @@
+console.log("Hello youtube stream");
+
+// variable declaration
+
+let username = "Hitesh Choudhary";
+username = "youtube";
+
+let userMobile = 9898989898;
+let isLoggedIn = true;
+
+let testMeLet;
+// var testMeVar : Not a recommended way to declare variables
+
+// console.log(testMeLet);
+// console.log(testMeVar);
+
+// TEXTS
+
+// var smiley = "\u"
+
+// comments
+
+/*
+easy
+everything that you write here is an comment
+
+*/
+
+// literals
+
+/*
+
+2323
+23.5
+"stirng type 'single quote' one"
+'string type "inside" two'
+true
+false
+null
+
+*/
+
+// reserved keywords
+
+// enum
+// implements
+// interface
+// package
+// private
+// protected
+// public
+// More are there but these are future reserved keywords
+
+console.log("\u00E9");
+console.log("\u0301");
+
+const café = 1;
+const café = 2;
+
+console.log(café);
+
+// semi colons
+
+let score = 0;
+[score, score + 3, score + 5].forEach(console.log);
